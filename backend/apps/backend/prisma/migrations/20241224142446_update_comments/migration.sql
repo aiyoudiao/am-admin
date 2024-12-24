7927dcece858cb178d6aeeea99f17fe6a5dff97d
aiyoudiao <newdiao@163.com>
2024-12-24 23:13:31 +0800

    ✨ feat: 添加 .gitignore 文件，更新 package.json 以支持 Prisma 生成和数据库迁移，新增 Prisma 数据库注释生成脚本

---
+++ backend/apps/backend/prisma/migrations/20241224142446_update_comments/migration.sql
@@ -0,0 +1,31 @@
+-- Prisma Database Comments Generator v1.0.2
+
+-- am_already comments
+COMMENT ON TABLE "am_already" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_announcement comments
+COMMENT ON TABLE "am_announcement" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_international comments
+COMMENT ON TABLE "am_international" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_jobs comments
+COMMENT ON TABLE "am_jobs" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_logs comments
+COMMENT ON TABLE "am_logs" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_menu comments
+COMMENT ON TABLE "am_menu" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_organization comments
+COMMENT ON TABLE "am_organization" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_permission comments
+COMMENT ON TABLE "am_permission" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_role comments
+COMMENT ON TABLE "am_role" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
+
+-- am_user comments
+COMMENT ON TABLE "am_user" IS 'This model or at least one of its fields has comments in the database, and requires an additional setup for migrations: Read more: https://pris.ly/d/database-comments';
